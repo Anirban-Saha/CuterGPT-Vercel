ef1fac400c6daffd4e8cb27bba207c1675c71e86
Anirban Saha <88977035+Anirban-Saha@users.noreply.github.com>
2023-09-09 21:14:08 +0530

    .

--- cuter-gpt/src/app/page.tsx
+++ cuter-gpt/src/app/page.tsx
@@ -1,23 +1,25 @@
 "use client";
+import React from "react"; 
 import { useChat } from "ai/react";
+
 export default function Home() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
+  
   return (
     <div>
-      {messages.map((message) => {
-        return (
-          <span key={message.id}>
-            {message.role} : {message.content}
-          </span>
-        );
-      })}
+      {messages.map((message) => (
+        <span key={message.id}>
+          {message.role} : {message.content}
+        </span>
+      ))}
       <form onSubmit={handleSubmit}>
         <input
+          type="text" 
           value={input}
-          onChange={handleInputChange}
-          
-          placeholder="Enter a message"
+          onChange={handleInputChange} 
+          placeholder="Enter text"
         />
+        <button type="submit">Send</button>
       </form>
     </div>
   );
